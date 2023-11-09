8dad5d960a63160324a76b59195955f38678a385
Tina Chen <tinachen@GinnywudeMacBook-Pro.local>
2023-11-09 16:06:03 +0800

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ["./dist/*.{html,js}","*.html","./js/*.{html,js}","node_modules/preline/dist/*.js","node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '480px'
+      },
+      colors: {
+        custom: {
+          darkBrown: '#3A3532',
+          brown: '#806A58',
+          beige: '#D6CDC6',
+          gray: '#F1F1F1'
+        }
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+        '1/5': '20%',
+        '4/5': '80%',
+        '5/6': '83%',
+        '6/5': '120%',
+        '1/10': '10%',
+        '1/20': '5%',
+      },
+      minHeight: {
+        '1/2': '50%',
+      },
+      height: {
+        'slider': '480px',
+      },
+      zIndex: {
+        '100': '100',
+      },
+      backgroundSize: {
+        '70%': '70%',
+        '50%': '50%'
+      }
+    },
   },
   plugins: [
     // require('@tailwindcss/forms'),
